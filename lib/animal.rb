3bb5f924d2aac776553a45cf239662e4e5da01ac
Alexander Jacks and Stephanie Gurung <alexander@>
2016-07-12 13:47:50 -0700

    pass Animal spec sort_by_breed method

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,5 +1,5 @@
 class Animal
-  attr_reader(:id, :name, :entry_date, :gender, :type, :breed, :friend_id)
+  attr_reader(:name, :entry_date, :gender, :type, :breed, :friend_id)
 
   define_method (:initialize) do |attributes|
     @name = attributes.fetch(:name)
@@ -10,12 +10,15 @@ class Animal
     @friend_id = attributes.fetch(:friend_id)
   end
 
-  define_method(:==) do |another_animal|
-      self.name.==(another_animal.name).&(self.entry_date.==another_animal.entry_date).&(self.type.==another_animal.type).&(self.breed.==another_animal.breed)
-    end
+  define_method(:==) do |other| #= mod'ed method from course reading: note =
+    same_name = self.name().eql?(other.name())
+     same_entry_date = self.entry_date().eql?(other.entry_date())
+     same_breed = self.breed().eql?(other.breed())
+     same_entry_date.&(same_name).&(same_breed)
+   end
 
   define_singleton_method (:all) do
-    returned_animals = DB.exec("SELECT * FROM animals;")
+    returned_animals = DB.exec("SELECT name, cast(entry_date AS date), gender, type, breed, friend_id FROM animals;")
     animals = []
     returned_animals.each() do |animal|
       name = animal.fetch('name')
@@ -29,7 +32,22 @@ class Animal
     animals
   end
 
-  define_method(:save) do 
+  define_method(:save) do
     DB.exec("INSERT INTO animals (name,entry_date,gender,type,breed,friend_id) VALUES ('#{@name}','#{@entry_date}','#{@gender}','#{@type}','#{@breed}',#{@friend_id});")
+  end
+
+  define_singleton_method(:sort_by_breed) do
+    animals = []
+    returned_animals = DB.exec("SELECT name, cast(entry_date AS date), gender, type, breed, friend_id FROM animals ORDER BY breed ASC;")
+    returned_animals.each() do |animal|
+      name = animal.fetch("name")
+      entry_date = animal.fetch("entry_date")
+      gender = animal.fetch("gender")
+      type = animal.fetch("type")
+      breed = animal.fetch("breed")
+      friend_id = animal.fetch("friend_id")
+      animals.push(Animal.new({:name => name, :entry_date => entry_date, :gender => gender, :type => type, :breed => breed, :friend_id => friend_id}))
+    end
+    animals
   end
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -11,13 +11,17 @@ describe(Animal) do
 
   describe('.all') do
     it "returns an empty array at first" do
-    expect(Animal.all()).to(eq([]))
+      test_animal1 = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => 1})
+      test_animal1.save()
+      test_animal2 = Animal.new({:name => 'Caw', :entry_date => '2016-07-13', :gender => 'f', :type => 'bird', :breed => 'goldfinch', :friend_id => 1})
+      test_animal2.save()
+      expect(Animal.all).to(eq([test_animal1,test_animal2]))
     end
   end
 
   describe('#save') do
     it "saves an animal into the database" do
-      test_animal = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => NULL})
+      test_animal = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => 1})
       test_animal.save()
       expect(Animal.all()).to(eq([test_animal]))
     end
@@ -25,18 +29,20 @@ describe(Animal) do
 
   describe '#==' do
     it "compares two animals to decide if they are the same animal" do
-      test_animal1 = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => nil})
-      test_animal_too = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => nil})
+      test_animal1 = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => 1})
+      test_animal_too = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => 1})
       test_animal1.eql?(test_animal_too)
     end
   end
 
+  describe '.sort_by_breed' do
+    it "returns a list of animals sorted by breed" do
+      test_animal1 = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => 1})
+      test_animal1.save()
+      test_animal2 = Animal.new({:name => 'Caw', :entry_date => '2016-07-13', :gender => 'f', :type => 'bird', :breed => 'goldfinch', :friend_id => 1})
+      test_animal2.save()
+      expect(Animal.sort_by_breed).to(eq([test_animal2,test_animal1]))
+    end
+  end
 
-
-  # describe('#id') do
-  #   it "returns the gender of the animal" do
-  #   test_animal = Animal.new({:id => 1, :name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => nil})
-  #   expect(test_animal.id()).to(eq(1))
-  #   end
-  # end
 end
