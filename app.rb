571605a8aa2e5445696653f41a7c46a53683278b
Alexander Jacks and Stephanie Gurung <alexander@>
2016-07-12 16:57:44 -0700

    builds some views

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/animal")
 require("./lib/friend")
+
+get('/') do
+  erb(:index)
+end
+
+get('/admin') do
+  erb(:admin)
+end
+
+get('/admin/animals/new') do
+  erb(:animal_form)
+end
