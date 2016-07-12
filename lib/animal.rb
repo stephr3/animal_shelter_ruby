9cc9682830d2ffb96678225a333de7519085b4ca
Alexander Jacks and Stephanie Gurung <alexander+stephanie@>
2016-07-12 10:41:46 -0700

    pass Animal spec .all

--- lib/animal.rb
+++ lib/animal.rb
@@ -10,7 +10,26 @@ class Animal
     @friend_id = attributes.fetch(:friend_id)
   end
 
+  define_method(:==) do |another_animal|
+      self.name.==(another_animal.name).&(self.entry_date.==another_animal.entry_date).&(self.type.==another_animal.type).&(self.breed.==another_animal.breed)
+    end
+
   define_singleton_method (:all) do
-    []
+    returned_animals = DB.exec("SELECT * FROM animals;")
+    animals = []
+    returned_animals.each() do |animal|
+      name = animal.fetch('name')
+      entry_date = animal.fetch('entry_date')
+      gender = animal.fetch('gender')
+      type = animal.fetch('type')
+      breed = animal.fetch('breed')
+      friend_id = animal.fetch('friend_id').to_i()
+      animals.push(Animal.new({:name => name, :entry_date => entry_date, :gender => gender, :type => type, :breed => breed, :friend_id => friend_id}))
+    end
+    animals
+  end
+
+  define_method(:save) do 
+    DB.exec("INSERT INTO animals (name,entry_date,gender,type,breed,friend_id) VALUES ('#{@name}','#{@entry_date}','#{@gender}','#{@type}','#{@breed}',#{@friend_id});")
   end
 end
--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -8,11 +8,30 @@ describe(Animal) do
     expect(test_animal.name()).to(eq('Boo'))
     end
   end
+
   describe('.all') do
     it "returns an empty array at first" do
     expect(Animal.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it "saves an animal into the database" do
+      test_animal = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => NULL})
+      test_animal.save()
+      expect(Animal.all()).to(eq([test_animal]))
+    end
+  end
+
+  describe '#==' do
+    it "compares two animals to decide if they are the same animal" do
+      test_animal1 = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => nil})
+      test_animal_too = Animal.new({:name => 'Boo', :entry_date => '2016-07-12', :gender => 'f', :type => 'fish', :breed => 'goldfish', :friend_id => nil})
+      test_animal1.eql?(test_animal_too)
+    end
+  end
+
+
 
   # describe('#id') do
   #   it "returns the gender of the animal" do
